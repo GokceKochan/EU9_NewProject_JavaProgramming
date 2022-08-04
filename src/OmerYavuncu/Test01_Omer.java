06aeeb60973328ffb04e513b26ebd6252c78c94d
omerf <omeryavuncu@gmail.com>
2022-08-04 14:01:01 +0100

    Created first test class

---
+++ src/OmerYavuncu/Test01_Omer.java
@@ -0,0 +1,15 @@
+package src.OmerYavuncu;
+
+public class Test01_Omer {
+
+    public static void main(String[] args) {
+
+        // "I added a package on my name"
+
+        System.out.println("-----------------------------");
+
+        // "I created a first test class for our project"
+
+    }
+
+}
