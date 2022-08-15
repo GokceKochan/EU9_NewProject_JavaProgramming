3295c62037f4ddeba014c1ae1325fce6e20104c1
gokceeksioglu <gokceeksioglu@hotmail.com>
2022-08-15 23:46:29 +0100

    Quiz Question

---
+++ src/GokceFolder/QuizQuestion.java
@@ -0,0 +1,35 @@
+package GokceFolder;
+
+import java.util.Scanner;
+
+public class QuizQuestion {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("what is the farthest planet in the solar system:" );
+        System.out.println("a)venus" );
+        System.out.println("b)pluto" );
+        System.out.println("c)neptune" );
+
+        String a = s.nextLine();
+
+        switch (a){
+            case "a":
+                System.out.println("a is wrong");
+                break;
+            case "b":
+                System.out.println("b is correct");
+                break;
+            case "c":
+                System.out.println("c is correct");
+            default:
+                System.out.println(a+" invalid planet");
+                break;
+
+
+        }
+
+
+
+    }
+}
